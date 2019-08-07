4ef9397fab02029785ece1afcfd8c8716b09422c
dvd <demeter@utexas.edu>
2019-08-07 12:07:12 -0700

    updated filter function to be in functions

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,9 +1,11 @@
-from .utils import query_api, load_groupfile#, normalize?, integrate/merg?
+from .utils import filterIQM, load_groupfile, merge_dfs, query_api
 from .figs import scatter 
 
     
-# __all__ = [
-#     'load_groupfile,'
-#     'query_api',
-#     'scatter',
-# ]
+__all__ = [
+    'filterIQM',
+    'load_groupfile,'
+    'merge_dfs',
+    'query_api',
+    'scatter',
+]
