0c7bf113217a41ea33f7cecfcb0221b3c765c825
dvd <demeter@utexas.edu>
2019-08-07 13:35:00 -0700

    add vioplot to init file and wrapper for importing

--- MRIQCeption_wrapper.py
+++ MRIQCeption_wrapper.py
@@ -15,7 +15,7 @@ __status__ = 'pre-alpha'
 """
 
 import argparse,datetime,os,sys,time
-from tools import load_groupfile, query_api, filterIQM
+from tools import load_groupfile, query_api, filterIQM, make_vio_plot
 
 #################################################
 ##             MAIN SCRIPT ENTRY               ##
