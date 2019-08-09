ff7058f738e4c7d35cdf110f50e96ee90bae33d2
Saren Seeley <sarenseeley@email.arizona.edu>
2019-08-09 11:47:43 -0700

    update code for wikiplot1.py

--- docs/wikiplot1.py
+++ docs/wikiplot1.py
@@ -16,62 +16,62 @@ import plotly.plotly as py
 from plotly.graph_objs import *
 py.sign_in('username', 'api_key')
 trace1 = {
-    "line": {"color": "rgba(31,119,180,1)"}, 
-    "name": "data", 
-    "type": "box", 
-    "xsrc": "sarenseeley:20:4e046e", 
-    "x": ["example 1", "example 1", "example 1", "example 1", "example 1", "example 1", "example 1", "example 1", "example 1", "example 1", "example 1", "example 1", "example 1", "example 1", "example 1", "example 1", "example 1", "example 1"], 
-    "ysrc": "sarenseeley:20:ab17b2", 
-    "y": [0.062761749, 0.069548301, 1.2, 0.106722959, 0.107913332, 0.098811028, 0.147040802, 0.145063702, 0.124137486, 0.085251371, 0.102119294, 0.110388611, 0.061137365, 0.06445587, 0.060540812, 0.040621786, 0.041287255, 0.042188746], 
-    "frame": None, 
-    "xaxis": "x", 
-    "yaxis": "y", 
-    "jitter": 0.3, 
-    "marker": {
-      "line": {
-        "color": "black", 
-        "width": 1
+      "line": {"color": "rgba(31,119,180,1)"}, 
+      "name": "data", 
+      "type": "box", 
+      "xsrc": "sarenseeley:20:4e046e", 
+      "x": ["example 1", "example 1", "example 1", "example 1", "example 1", "example 1", "example 1", "example 1", "example 1", "example 1", "example 1", "example 1", "example 1", "example 1", "example 1", "example 1", "example 1", "example 1"], 
+      "ysrc": "sarenseeley:20:ab17b2", 
+      "y": [0.062761749, 0.069548301, 1.2, 0.106722959, 0.107913332, 0.098811028, 0.147040802, 0.145063702, 0.124137486, 0.085251371, 0.102119294, 0.110388611, 0.061137365, 0.06445587, 0.060540812, 0.040621786, 0.041287255, 0.042188746], 
+      "frame": None, 
+      "xaxis": "x", 
+      "yaxis": "y", 
+      "jitter": 0.3, 
+      "marker": {
+        "line": {
+          "color": "black", 
+          "width": 1
+        }, 
+        "size": 12, 
+        "color": "rgba(31,119,180,1)"
       }, 
-      "size": 12, 
-      "color": "rgba(31,119,180,1)"
-    }, 
-    "pointpos": -1.8, 
-    "boxpoints": "all", 
-    "fillcolor": "rgba(31,119,180,0.5)"
-  }
+      "pointpos": -1.8, 
+      "boxpoints": "all", 
+      "fillcolor": "rgba(31,119,180,0.5)"
+    }
 data = Data([trace1])
 layout = {
-    "xaxis": {
-      "type": "category", 
-      "title": "data", 
-      "domain": [0, 1], 
-      "titlefont": {
-        "size": 18, 
-        "color": "black", 
-        "family": "Arial, sans-serif"
+      "xaxis": {
+        "type": "category", 
+        "title": "data", 
+        "domain": [0, 1], 
+        "titlefont": {
+          "size": 18, 
+          "color": "black", 
+          "family": "Arial, sans-serif"
+        }, 
+        "automargin": True, 
+        "categoryarray": ["example 1"], 
+        "categoryorder": "array"
       }, 
-      "automargin": True, 
-      "categoryarray": ["example 1"], 
-      "categoryorder": "array"
-    }, 
-    "yaxis": {
-      "title": "fd_mean", 
-      "domain": [0, 1], 
-      "titlefont": {
-        "size": 18, 
-        "color": "black", 
-        "family": "Arial, sans-serif"
+      "yaxis": {
+        "title": "fd_mean", 
+        "domain": [0, 1], 
+        "titlefont": {
+          "size": 18, 
+          "color": "black", 
+          "family": "Arial, sans-serif"
+        }, 
+        "automargin": True
       }, 
-      "automargin": True
-    }, 
-    "margin": {
-      "b": 40, 
-      "l": 60, 
-      "r": 10, 
-      "t": 25
-    }, 
-    "hovermode": "closest", 
-    "showlegend": False
-  }
+      "margin": {
+        "b": 40, 
+        "l": 60, 
+        "r": 10, 
+        "t": 25
+      }, 
+      "hovermode": "closest", 
+      "showlegend": False
+    }
 fig = Figure(data=data, layout=layout)
 plot_url = py.plot(fig)
